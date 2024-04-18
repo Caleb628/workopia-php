be799207bf383b5a9b6bf740154f4e666fd55fd2
Caleb Dourado <caledourado3@gmail.com>
2024-04-18 10:54:17 -0300

    helpers and partials created

--- helpers.php
+++ helpers.php
@@ -10,3 +10,58 @@ function basePath($path ='')
 {
 return __DIR__ . '/' . $path;
 };
+/*
+*load a view
+*
+* @param string $name
+* @return  void
+*/
+function loadView($name)
+{
+$viewPath = basePath("views/{$name}.views.php");
+if(file_exists($viewPath)){
+ require $viewPath;
+}else {
+    echo "file '{$name} not found'";
+}
+};
+/*
+*load a partial
+*
+* @param string $name
+* @return  void
+*/
+function loadPartial($name)
+{
+$partialPath = basePath("views/partials/{$name}.php");
+
+if(file_exists($partialPath)){
+    require $partialPath;
+   }else {
+       echo "file '{$name} not found'";
+   }
+};
+/*
+*Inspect a value(s)
+*
+* @param string $name
+* @return  void
+*/
+function inspect($value)
+{
+echo '<pre>';
+var_dump($value);
+echo '</pre>';
+}
+/*
+*Inspect a value(s) and die
+*
+* @param string $name
+* @return  void
+*/
+function inspectAndDie($value)
+{
+echo '<pre>';
+die(var_dump($value));
+echo '</pre>';
+}
